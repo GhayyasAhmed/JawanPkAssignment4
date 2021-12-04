73b6da3aa6dedca9db6efa02f2bad77c30aa8c88
GhayyasAhmed <ghayyas1ahmed@gmail.com>
2021-12-04 18:06:32 +0500

    Update program1

--- assignment4.py
+++ assignment4.py
@@ -1,34 +1,34 @@
 
 
 # Program 1
-
-
-num1 = float(input("Enter number 1: "))
 print("Enter what operation you want to perform: ")
-
+operators = ["+", "-", "*", "/", "**"]
 operator = input("Addition(+), Subtraction(-), Multiplication(*), Division(/), Power(**): ")
 
-num2 = float(input("Enter number 2: "))
+if operator in operators:
+    num1 = float(input("Enter number 1: "))
 
-if operator == "+":
-    res = num1 + num2
-    print(num1,"+",num2,"=",res)
+    num2 = float(input("Enter number 2: "))
 
-elif operator == "-":
-    res = num1 - num2
-    print(num1,"-",num2,"=",res)
+    if operator == "+":
+        res = num1 + num2
+        print(num1,"+",num2,"=",res)
 
-elif operator == "*":
-    res = num1 * num2
-    print(num1,"*",num2,"=",res)
+    elif operator == "-":
+        res = num1 - num2
+        print(num1,"-",num2,"=",res)
 
-elif operator == "/":
-    res = num1 / num2
-    print(num1,"/",num2,"=",res)
+    elif operator == "*":
+        res = num1 * num2
+        print(num1,"*",num2,"=",res)
 
-elif operator == "**":
-    res = num1 ** num2
-    print(num1,"**",num2,"=",res)
+    elif operator == "/":
+        res = num1 / num2
+        print(num1,"/",num2,"=",res)
+
+    elif operator == "**":
+        res = num1 ** num2
+        print(num1,"**",num2,"=",res)
 
 else:
     print("Invalid operator")
